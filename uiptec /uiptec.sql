55d65055f624e0d8ca89bdd349323049c4fb8e83
shelsxacm <misscadxverous@gmail.com>
2016-08-20 04:49:40 -0500

    finishing queries

--- uiptec /uiptec.sql
+++ uiptec /uiptec.sql
@@ -31,5 +31,33 @@ SELECT * FROM Articulos ORDER BY id_fabricante ASC;
 -- Seleccionar todos los datos de los articulos cuyo precio esta entre 50$ y 100$
 SELECT id AS Código, Nombre as Articulo, Precio FROM Articulos
 WHERE Precio >= 50 AND Precio <= 100;
+-- Seleccionar todos los datos de los artículos cuyo precio está entre $50 y $100.
+
+-- Seleccionar el precio promedio de los artículos cuyo fabricante tenga el código 3.
+SELECT AVG(Precio) FROM Articulos  
+WHERE id_fabricante = 3;
+
+-- Seleccionar la cantidad de artículos cuyo precio sea menor a $175.
+SELECT COUNT(Precio) as Cantidad_Articulos 
+FROM Articulos 
+WHERE Precio < 175;
+
+-- Seleccionar una lista con el nombre y precio de los artículos más caros de cada proveedor.
+SELECT id_fabricante AS Código_Fabricante, id AS Código_Articulo, Nombre AS Articulo, MAX(Precio) AS Precio  
+FROM Articulos 
+GROUP BY id_fabricante ASC, id, Nombre LIMIT 1,3;
+
+
+
+
+
+
+
+-- Cambiar el nombre del producto 2 a Impresora.
+UPDATE Articulos 
+SET Nombre = 'Impresora'
+WHERE id = '1602';
+
+
 
 
